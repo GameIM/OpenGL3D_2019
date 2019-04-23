1afdb693b0306bbd8082141c291b17468d54dbb1
cv3m9acx5 <cv3m9acx5@outlook.jp>
2019-04-23 21:51:55 +0900

    シーンクラスの定義を作成

--- Src/Scene.h
+++ Src/Scene.h
@@ -1,1 +1,44 @@
-#pragma once
+/**
+* @file scene.h
+*/
+#ifndef SCENE_H_INCLUDED
+#define SCENE_H_INCLUDED
+#include <memory>
+#include <iostream>
+
+class SceneStack;//相互参照
+
+/**
+* シーンの基底クラス
+*/
+class Scene
+{
+public:
+	Scene(const char* name);//コンストラクタ
+	Scene(const Scene&) = delete;//コピーコンストラクタ
+	Scene& operator = (const Scene&) = delete;
+	virtual ~Scene();//デストラクタ
+
+	//純粋仮想関数
+	virtual bool Initialize() = 0{}
+	virtual void ProcessInput() = 0{}
+	virtual void Update(float) = 0{}
+	virtual void Render() = 0{}
+	virtual void Finalize() = 0{}
+
+	//仮想関数
+	virtual void Play();
+	virtual void Stop();
+	virtual void Show();
+	virtual void Hide();
+	const std::string& Name() const;
+	bool IsActive() const;
+	bool IsVisible() const;
+
+private:
+	std::string name;
+	bool isVisible = true;
+	bool isActive = true;
+};
+using ScenePtr = std::shared_ptr<Scene>;
+#endif //SCENE_H_INCLUDED
