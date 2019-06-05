07a26f714b73f702530ecf0068a5e2e9864a4d46
cv3m9acx5 <cv3m9acx5@outlook.jp>
2019-06-05 19:11:04 +0900

    定義を追加

--- Src/TitleScene.h
+++ Src/TitleScene.h
@@ -4,6 +4,9 @@
 #ifndef _TITLESCENE_H_INCLUDED
 #define _TITLESCENE_H_INCLUDED
 #include "Scene.h"
+#include "Sprite.h"
+#include "Font.h"
+#include <vector>
 
 class TitleScene : public Scene
 {
@@ -11,10 +14,16 @@ public:
 	TitleScene() : Scene("TitleScene"){}
 	virtual ~TitleScene() = default;
 
-	virtual bool Initialize() override { return true; }
+	virtual bool Initialize() override;
 	virtual void ProcessInput() override;
-	virtual void Update(float)override {};
-	virtual void Render()override {};
-	virtual void Finalize()override {};
+	virtual void Update(float) override;
+	virtual void Render() override;
+	virtual void Finalize() override {};
+
+private:
+	bool flag = false;
+	std::vector<Sprite> sprites;
+	SpriteRenderer spriteRenderer;
+	FontRenderer fontRenderer;
 };
 #endif//TITLESCENE_H_INCLUDED
