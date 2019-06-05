79f6852edd81c199d57ba93bbd7227c9a076c7eb
cv3m9acx5 <cv3m9acx5@outlook.jp>
2019-06-05 19:50:30 +0900

    定義を追加

--- Src/Scene.h
+++ Src/Scene.h
@@ -1,36 +1,35 @@
 /**
-* @file scene.h
+* @file Scene.h
 */
 #ifndef SCENE_H_INCLUDED
 #define SCENE_H_INCLUDED
 #include <memory>
 #include <string>
+#include <vector>
 
-class SceneStack;//相互参照
-
+class SceneStack;
 /**
 * シーンの基底クラス
 */
 class Scene
 {
 public:
-	Scene(const char* name);//コンストラクタ
-	Scene(const Scene&) = delete;//コピーコンストラクタ
+	Scene(const char* name);
+	Scene(const Scene&) = delete;
 	Scene& operator = (const Scene&) = delete;
-	virtual ~Scene();//デストラクタ
+	virtual ~Scene();
 
-	//純粋仮想関数
-	virtual bool Initialize() = 0{}
-	virtual void ProcessInput() = 0{}
-	virtual void Update(float) = 0{}
-	virtual void Render() = 0{}
-	virtual void Finalize() = 0{}
+	virtual bool Initialize() = 0 {}
+	virtual void ProcessInput() = 0 {}
+	virtual void Update(float) = 0 {}
+	virtual void Render() = 0 {}
+	virtual void Finalize() = 0 {}
 
-	//仮想関数
 	virtual void Play();
 	virtual void Stop();
 	virtual void Show();
 	virtual void Hide();
+
 	const std::string& Name() const;
 	bool IsActive() const;
 	bool IsVisible() const;
@@ -41,4 +40,32 @@ private:
 	bool isActive = true;
 };
 using ScenePtr = std::shared_ptr<Scene>;
+
+/**
+* シーン管理クラス
+*/
+class SceneStack
+{
+public:
+	static SceneStack& Instance();
+
+	void Push(ScenePtr);
+	void Pop();
+	void Replace(ScenePtr);
+	Scene& Current();
+	const Scene& Current() const;
+	size_t Size() const;
+	bool Empty() const;
+
+	void Update(float);
+	void Render();
+
+private:
+	SceneStack();
+	SceneStack(const SceneStack&) = delete;
+	SceneStack& operator = (const SceneStack&) = delete;
+	~SceneStack() = default;
+
+	std::vector<ScenePtr> stack;
+};
 #endif //SCENE_H_INCLUDED
