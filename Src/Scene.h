7a5ce013849984a27670337d6f2643b153fbe67f
cv3m9acx5 <cv3m9acx5@outlook.jp>
2019-06-05 19:15:46 +0900

    Revert "ファイルを定義する"
    This reverts commit 5abb79daf60264f04a3737c4f392c67cdc636317.

--- Src/Scene.h
+++ Src/Scene.h
@@ -1,35 +1,36 @@
 /**
-* @file Scene.h
+* @file scene.h
 */
 #ifndef SCENE_H_INCLUDED
 #define SCENE_H_INCLUDED
 #include <memory>
 #include <string>
-#include <vector>
 
-class SceneStack;
+class SceneStack;//相互参照
+
 /**
 * シーンの基底クラス
 */
 class Scene
 {
 public:
-	Scene(const char* name);
-	Scene(const Scene&) = delete;
+	Scene(const char* name);//コンストラクタ
+	Scene(const Scene&) = delete;//コピーコンストラクタ
 	Scene& operator = (const Scene&) = delete;
-	virtual ~Scene();
+	virtual ~Scene();//デストラクタ
 
-	virtual bool Initialize() = 0 {}
-	virtual void ProcessInput() = 0 {}
-	virtual void Update(float) = 0 {}
-	virtual void Render() = 0 {}
-	virtual void Finalize() = 0 {}
+	//純粋仮想関数
+	virtual bool Initialize() = 0{}
+	virtual void ProcessInput() = 0{}
+	virtual void Update(float) = 0{}
+	virtual void Render() = 0{}
+	virtual void Finalize() = 0{}
 
+	//仮想関数
 	virtual void Play();
 	virtual void Stop();
 	virtual void Show();
 	virtual void Hide();
-
 	const std::string& Name() const;
 	bool IsActive() const;
 	bool IsVisible() const;
@@ -40,32 +41,4 @@ private:
 	bool isActive = true;
 };
 using ScenePtr = std::shared_ptr<Scene>;
-
-/**
-* シーン管理クラス
-*/
-class SceneStack
-{
-public:
-	static SceneStack& Instance();
-
-	void Push(ScenePtr);
-	void Pop();
-	void Replace(ScenePtr);
-	Scene& Current();
-	const Scene& Current() const;
-	size_t Size() const;
-	bool Empty() const;
-
-	void Update(float);
-	void Render();
-
-private:
-	SceneStack();
-	SceneStack(const SceneStack&) = delete;
-	SceneStack& operator = (const SceneStack&) = delete;
-	~SceneStack() = default;
-
-	std::vector<ScenePtr> stack;
-};
 #endif //SCENE_H_INCLUDED
